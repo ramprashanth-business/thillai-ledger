73dc5060b10513165168b0e6b27e67a113f9dfd7
ramprashanth-business <ramprashanth2001@gmail.com>
2026-08-08 13:17:12 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'thillai-ledger-v1';
+const CACHE_NAME = 'thillai-ledger-v4';
 const SHELL_FILES = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
